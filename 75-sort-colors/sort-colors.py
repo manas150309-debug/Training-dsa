adaec112891e2f73a73e9e239ba3e1cedad6aa36
manas150309-debug <manas150309@gmail.com>
2026-02-07 15:22:54 +0530

    Time: 3 ms (11.70%) | Memory: 19.3 MB (36.58%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortColors(self, nums):
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(n - i-1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+        
