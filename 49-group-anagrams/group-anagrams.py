8b8c2c48e5145a479750574f9691f1842926dc85
manas150309-debug <manas150309@gmail.com>
2026-02-11 09:28:19 +0530

    Time: 15 ms (48.79%) | Memory: 24.2 MB (8.33%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - ord('a')] += 1
+            key = tuple(count)
+            anagram_map[key].append(word)
+
+        return list(anagram_map.values())
+     
